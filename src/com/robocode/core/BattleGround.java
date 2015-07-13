685a8e9ddad4a119bbe6e4f669616194bdea9107
liyiang1268 <liyiang1268@126.com>
2015-07-13 10:37:12 +0800

    战场类

--- src/com/robocode/core/BattleGround.java
+++ src/com/robocode/core/BattleGround.java
@@ -1,7 +1,28 @@
 package com.robocode.core;
+import java.awt.*;
+import java.awt.event.*;
 /**
  * ºÚΩÈ£∫’Ω≥°¿‡
  */
-public class BattleGround {
-
+public class BattleGround extends Frame{
+	public static final int GROUND_WIDTH = 800;
+	public static final int GROUND_HEIGHT = 600;
+	
+	public void ground(){
+		this.setBackground(new Color(0,255,255));
+		this.setSize(GROUND_WIDTH, GROUND_HEIGHT);
+		this.setTitle("RoboCode");
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		this.setResizable(false);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		BattleGround bg=new BattleGround();
+		bg.ground();
+	}
 }
